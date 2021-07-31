985bc19ffd4883e15c90e5e34f64db2ce9b494f9
toufik <toufikbeycgn@gmail.com>
2021-07-31 13:58:31 +0200

    User schema + User route regisration

---
+++ routes/app/users.js
@@ -0,0 +1,57 @@
+const express = require('express'); 
+const router = express.Router(); 
+const { check, validationResult} = require('express-validator/check');
+const User = require('../../modules/User');
+const gravatar = require('gravatar'); 
+// User registration 
+
+router.post('/',[
+    check('name','Name is required')
+    .not()
+    .isEmpty(),
+    check('email','please enter a correct email')
+    .isEmail(),
+    check('password','please enter a password with 6 or more characters')
+    .isLength({min:6})
+],
+async (req,res)=>{
+     const errors = validationResult(req); 
+     if(!errors.isEmpty()){
+         return res.status(400).json({errors : errors.array()}); 
+        }
+    const { name , email , password} = req.body; 
+
+    try {
+        let user = await User.findOne({email}); 
+        if(user) {
+            res.status(400).json({errors:[{msg:'User already exist'}]}); 
+        }; 
+        const avatar  = gravatar.url(email,{
+            s:'200',
+            r:'pg',
+            d:'mm'
+        })
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        }); 
+
+        await user.save(); 
+        res.send('User register'); 
+        
+
+    } catch (error) {
+        console.error(error.message)
+        
+    }
+
+
+    console.log(req.body); 
+    res.send('user route');
+});
+
+
+module.exports = router;  
--- server.js
+++ server.js
@@ -6,8 +6,17 @@ const app = express();
 connectDB();
 
 
+// bodyParser
+app.use(express.json({extended:false}));
 
 
+// bring the routes 
+app.use('/api/users', require('./routes/app/users'));
+app.use('/api/profile', require('./routes/app/profile'));
+app.use('/api/auth', require('./routes/app/auth'));
+app.use('/api/poste', require('./routes/app/poste'));
+
+// init the server
 app.get('/', (req,res)=> res.send('server runing'))
 const PORT = process.env.PORT  || 5000; 
 
